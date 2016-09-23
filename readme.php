c3df329574193af58dd3f493dadafe6bb4f203ff
simplon-sofianeD <dahmani.sof@gmail.com>
2016-09-23 17:37:06 +0200

    exo

--- readme.php
+++ readme.php
@@ -1,12 +1,15 @@
 Créer un nouveau dossier et créer un fichier index.php suivi du code ci-dessous:
 Liste des prochains exercices :
+Yacine :
 - popup
-- sidebar
 - transition
+- transition avec un pseudo element (before, after)
+Sofiane :
+- sidebar
 - animation
 - transition avec un pseudo element (before, after)
-- transition simultanée entre plusieurs div
-- chacune des divs comprenant une image photoshop
+Nassima :
+- transition simultanée entre plusieurs div chacune des divs comprenant une image photoshop
 -	Squelette de la nouvelle page :
 
 <?php
