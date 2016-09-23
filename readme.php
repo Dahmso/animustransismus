765d80f7f8c82aa4473e5f546f75c5db20e54ff4
simplon-sofianeD <dahmani.sof@gmail.com>
2016-09-23 17:34:26 +0200

    save

--- readme.php
+++ readme.php
@@ -1,7 +1,12 @@
 Créer un nouveau dossier et créer un fichier index.php suivi du code ci-dessous:
-
-
-
+Liste des prochains exercices :
+- popup
+- sidebar
+- transition
+- animation
+- transition avec un pseudo element (before, after)
+- transition simultanée entre plusieurs div
+- chacune des divs comprenant une image photoshop
 -	Squelette de la nouvelle page :
 
 <?php
