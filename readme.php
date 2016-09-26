8f1ac81ad4d2fb19557d18b08b5ffcd31f967be8
simplon-BYacine <yacinebelarbi@hotmail.fr>
2016-09-26 10:30:09 +0200

    Séparation du html et du js

--- readme.php
+++ readme.php
@@ -1,22 +1,8 @@
-Créer un nouveau dossier et créer un fichier index.php suivi du code ci-dessous:
-
-
-
--	Squelette de la nouvelle page :
-
-<?php
-// Nom du fichier css
-// $fichier_css = "style.css"
-$fichier_css = "style.css";
-
-include'../head.php';
-?>
-</div> <!-- END SIDEBAR -->
-
-<div class="page">
-
- <!--- CONTENU DE LA PAGE -->
-
-</div> <!-- END PAGE -->
-
-<?php include'../footer.php'; ?>
+Liste des prochains exercices :
+- popup
+- sidebar
+- transition
+- animation
+- transition avec un pseudo element (before, after)
+- transition simultanée entre plusieurs div 
+- chacune des divs comprenant une image photoshop
